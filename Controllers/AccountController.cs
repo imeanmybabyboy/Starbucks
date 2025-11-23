82d9786ceb4f3a72d451dc804a0e61f62812634f
imeanmybabyboy <pushenko.roman@gmail.com>
2025-11-23 14:47:10 +0200

    SignIn window added

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Starbucks.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            bool isWindowOpened = true;
+            ViewData["isWindowOpened"] = isWindowOpened;
+
+            return View();
+        }
+    }
+}
